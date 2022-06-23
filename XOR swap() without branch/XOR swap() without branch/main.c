7902129d3a44b13dec2e30cb98fc97fb639ed860
Ria9993 <hyunjun2372@gmail.com>
2022-06-23 12:13:16 +0900

    add C code in "XOR swap() without branch"

--- XOR swap() without branch/XOR swap() without branch/main.c
+++ XOR swap() without branch/XOR swap() without branch/main.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdint.h>
 
 /* 
 Reference : À¯¿µÃµ(Megayuchi)
@@ -8,43 +9,61 @@ https://megayuchi.com/2015/09/10/xor%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%98%EC%97%
 
 int main()
 {
-    int a = 5;
-    int b = 10;
+    int32_t a = 5;
+    int32_t b = 13;
 
-    /* Original C Code
+    int32_t ret_a;
+    int32_t ret_b;
+
+    printf("Original\n");
+    printf("a = %d, b = %d\n", a, b);
+
+    /* Original C Code */
     if (a < b)
     {
         //swap
-        int tmp = a;
-        a = b;
-        b = tmp;
-    } */
-    
-    // ASM Code without Branch
+        int32_t tmp = a;
+        ret_a = b;
+        ret_b = tmp;
+    }
+    printf("Original C Code\n");
+    printf("a = %d, b = %d\n", ret_a, ret_b);
+
+    /* ASM Code without Branch */
     __asm
     {
-        mov eax, [a]
-        mov ebx, [b]
+		mov eax, [a]
+		mov ebx, [b]
 
-        ; carry mask
-        xor ecx, ecx; ecx = 0
-        cmp eax, ebx
-        sbb ecx, 0; IF a < b, THEN 0xFFFFFFFF, ELSE 0
+		; carry mask
+		xor ecx, ecx; ecx = 0
+		cmp eax, ebx
+		sbb ecx, 0; IF a < b, THEN 0xFFFFFFFF, ELSE 0
 
-        ;xor mask
-        mov edx, eax;
-        xor edx, ebx; a^ b
-            and edx, ecx; IF a < b, THEN 0, ELSE a^ b
+		; xor mask
+		mov edx, eax;
+		xor edx, ebx; a ^ b
+		and edx, ecx; IF a < b, THEN a ^ b, ELSE 0
 
-            ; change
-            xor eax, edx
-            xor ebx, edx
+		; change
+		xor eax, edx
+		xor ebx, edx
 
-            mov[a], eax
-            mov[b], ebx
+		mov[ret_a], eax
+		mov[ret_b], ebx
     }
+    printf("ASM Code Without Branch\n");
+    printf("a = %d, b = %d\n", ret_a, ret_b);
 
-    printf("a = %d, b = %d", a, b);
+    /* C Code without Branch (by me) */
+    {
+        const int32_t carry_mask = (a - b) >> 31; // IF a < b, THEN 0xFFFFFFFF, ELSE 0
+        const int32_t xor_mask = (a ^ b) & carry_mask; // IF a < b, THEN a ^ b, ELSE 0
+        ret_a = a ^ xor_mask;
+        ret_b = b ^ xor_mask;
+    }
+    printf("ASM Code Without Branch\n");
+    printf("a = %d, b = %d\n", ret_a, ret_b);
 
     return 0;
 }
