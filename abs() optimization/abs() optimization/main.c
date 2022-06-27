179ad37d7ca81d849d9cbc3487ccf5f0f2dea8ef
Ria9993 <hyunjun2372@gmail.com>
2022-06-27 12:24:56 +0900

    Improve scalability

--- abs() optimization/abs() optimization/main.c
+++ abs() optimization/abs() optimization/main.c
@@ -1,11 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <limits.h>
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    int mask = n >> 31; // Bit filling by MSB
+    int mask = n >> ((CHAR_BIT * sizeof(int)) - 1); // Bit filling by MSB
 
     // Trans sign only negative number
     n ^= mask;
